9ad57085dba284c5c8302354304433551bce2236
Eduardo-Gv17 <eduardo.guevara@utec.edu.pe>
2025-10-21 18:20:21 -0500

    PROYECTAAA

---
+++ src/main/java/org/example/hackaton1/Hackaton1Application.java
@@ -0,0 +1,13 @@
+package org.example.hackaton1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackaton1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hackaton1/Hackaton1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackaton1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
