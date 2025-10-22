a1ad6f6b1b7f0d475181bf6ef538b118070bab79
Eduardo-Gv17 <eduardo.guevara@utec.edu.pe>
2025-10-21 19:07:19 -0500

    dtos

--- src/main/java/org/example/hackaton1/dto/AuthRequest.java
+++ src/main/java/org/example/hackaton1/dto/AuthRequest.java
@@ -1,4 +1,12 @@
 package org.example.hackaton1.dto;
 
 public class AuthRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
